3e1f51c09d6301bea5ff07801fddbbbcfc714121
棋 <imqi1@qq.com>
2026-04-05 15:51:18 +0800

    1

--- prisma/migrations/20260405075051_init/migration.sql
+++ prisma/migrations/20260405075051_init/migration.sql
@@ -16,8 +16,24 @@ CREATE TABLE "Subscribe" (
     "url" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "avatar" TEXT,
+    "lastUpdated" TIMESTAMP(3),
 
     CONSTRAINT "Subscribe_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "SubscribePost" (
+    "id" SERIAL NOT NULL,
+    "subscribeId" INTEGER NOT NULL,
+    "title" TEXT NOT NULL,
+    "link" TEXT NOT NULL,
+    "description" TEXT,
+    "content" TEXT,
+    "author" TEXT,
+    "pubDate" TIMESTAMP(3),
+    "create_time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "SubscribePost_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -34,6 +50,7 @@ CREATE TABLE "Changelog" (
 CREATE TABLE "Post" (
     "cid" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
+    "slug" TEXT,
     "desc" TEXT,
     "content" TEXT,
     "create_time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -43,6 +60,8 @@ CREATE TABLE "Post" (
     "many_covers" BOOLEAN NOT NULL DEFAULT false,
     "covers" TEXT,
     "show_toc" BOOLEAN NOT NULL DEFAULT true,
+    "tags" TEXT,
+    "uid" INTEGER NOT NULL DEFAULT 1,
 
     CONSTRAINT "Post_pkey" PRIMARY KEY ("cid")
 );
@@ -52,6 +71,7 @@ CREATE TABLE "Comment" (
     "coid" SERIAL NOT NULL,
     "cid" INTEGER NOT NULL,
     "name" TEXT NOT NULL,
+    "mail" TEXT,
     "link" TEXT,
     "avatar" TEXT,
     "content" TEXT NOT NULL,
@@ -65,9 +85,23 @@ CREATE TABLE "Comment" (
 );
 
 -- CreateTable
+CREATE TABLE "Attachment" (
+    "aid" SERIAL NOT NULL,
+    "cid" INTEGER NOT NULL,
+    "type" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "storage" TEXT NOT NULL DEFAULT 'local',
+    "create_time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Attachment_pkey" PRIMARY KEY ("aid")
+);
+
+-- CreateTable
 CREATE TABLE "Category" (
     "mid" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
+    "slug" TEXT,
     "desc" TEXT,
     "class" TEXT,
 
@@ -85,8 +119,9 @@ CREATE TABLE "PostRelation" (
 
 -- CreateTable
 CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
+    "uid" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
+    "nickname" TEXT,
     "avatar" TEXT,
     "mail" TEXT NOT NULL,
     "password" TEXT NOT NULL,
@@ -94,7 +129,7 @@ CREATE TABLE "User" (
     "auth_code" TEXT,
     "role" INTEGER NOT NULL DEFAULT 0,
 
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "User_pkey" PRIMARY KEY ("uid")
 );
 
 -- CreateTable
@@ -107,7 +142,16 @@ CREATE TABLE "Meta" (
 );
 
 -- CreateIndex
+CREATE UNIQUE INDEX "SubscribePost_link_key" ON "SubscribePost"("link");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Post_slug_key" ON "Post"("slug");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_slug_key" ON "Category"("slug");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "PostRelation_cid_mid_key" ON "PostRelation"("cid", "mid");
@@ -122,7 +166,16 @@ CREATE UNIQUE INDEX "User_mail_key" ON "User"("mail");
 CREATE UNIQUE INDEX "Meta_key_key" ON "Meta"("key");
 
 -- AddForeignKey
+ALTER TABLE "SubscribePost" ADD CONSTRAINT "SubscribePost_subscribeId_fkey" FOREIGN KEY ("subscribeId") REFERENCES "Subscribe"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_uid_fkey" FOREIGN KEY ("uid") REFERENCES "User"("uid") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "Comment" ADD CONSTRAINT "Comment_cid_fkey" FOREIGN KEY ("cid") REFERENCES "Post"("cid") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Attachment" ADD CONSTRAINT "Attachment_cid_fkey" FOREIGN KEY ("cid") REFERENCES "Post"("cid") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "PostRelation" ADD CONSTRAINT "PostRelation_cid_fkey" FOREIGN KEY ("cid") REFERENCES "Post"("cid") ON DELETE CASCADE ON UPDATE CASCADE;
