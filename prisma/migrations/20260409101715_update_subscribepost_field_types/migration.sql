3ead2a971e0d4faa1a70b47f1dae1e40d67bd12c
棋 <imqi1@qq.com>
2026-04-10 15:10:12 +0800

    123

--- prisma/migrations/20260409101715_update_subscribepost_field_types/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE `subscribepost` MODIFY `title` VARCHAR(500) NOT NULL,
-    MODIFY `link` VARCHAR(500) NOT NULL,
-    MODIFY `description` TEXT NULL,
-    MODIFY `content` LONGTEXT NULL,
-    MODIFY `author` VARCHAR(255) NULL;
