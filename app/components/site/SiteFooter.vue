4756d6c4ec29f673bd5af29cc7eec2361a22cfb7
棋 <imqi1@qq.com>
2026-04-17 23:25:36 +0800

    fix(SiteFooter): 修正 z-index 样式错误

--- app/components/site/SiteFooter.vue
+++ app/components/site/SiteFooter.vue
@@ -221,7 +221,7 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div class="bg-slate-50 dark:bg-slate-900 z-9999">
+  <div class="bg-slate-50 dark:bg-slate-900 z-9">
     <div class="flex items-center justify-between p-5 max-w-175 w-full mx-auto font-semibold text-slate-600 dark:text-slate-400 max-sm:flex-col gap-3">
       <div class="flex items-center gap-2">
         <span>{{ currentYear }} &copy; {{ siteName }}</span>
