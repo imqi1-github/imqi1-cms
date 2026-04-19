0da4f5e960c13ec5b7b7ee482527f8d7df9935b1
棋 <imqi1@qq.com>
2026-04-19 18:48:37 +0800

    feat(cache): 配置 Redis 缓存并添加连接参数

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -265,11 +265,17 @@ export default defineNuxtConfig({
         port: Number(process.env.REDIS_PORT) || 6379,
         password: process.env.REDIS_PASSWORD,
         db: Number(process.env.REDIS_DB) || 0,
+        lazyConnect: false,
       },
 
       cache: process.env.REDIS_HOST
         ? {
             driver: "redis",
+            host: process.env.REDIS_HOST,
+            port: Number(process.env.REDIS_PORT) || 6379,
+            password: process.env.REDIS_PASSWORD,
+            db: Number(process.env.REDIS_DB) || 0,
+            lazyConnect: false,
           }
         : {
             driver: "fs",
